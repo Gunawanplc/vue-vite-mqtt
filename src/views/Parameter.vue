cce55f43b2aa30d97a944a6344c02b2a63afb1fc
Gunawan <gunawan.plc@gmail.com>
2026-02-02 00:56:06 +0700

    update feature

--- src/views/Parameter.vue
+++ src/views/Parameter.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="dashboard">
     <header style="color :white;padding:4px;background-color: black;font-size: 36px;font-weight: bold;">
-      <span>Water Gate System</span>
+      <span>Water Gate System 1</span>
       <!-- <div class="status-chip">System Online</div> -->
     </header>
 
